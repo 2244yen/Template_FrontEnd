e6feb4bc097ff737c9a1808be631919a177c6a95
LinhDang <linhdtt92@gmail.com>
2016-10-20 08:17:55 +0700

    edit bakery

--- Bakery/js/main.js
+++ Bakery/js/main.js
@@ -1,14 +1,5 @@
 
 $(document).ready(function() {
-
-	//Sort random function
-	function random(owlSelector){
-	    owlSelector.children().sort(function(){
-	        return Math.round(Math.random()) - 0.5;
-	    }).each(function(){
-	      $(this).appendTo(owlSelector);
-	    });
-	}
 	 
 	$("#owl-bakery").owlCarousel({
 	    nav: true,
@@ -22,13 +13,9 @@ $(document).ready(function() {
 	 	itemsDesktop:[1199,3], 
 	 	itemsDesktopSmall:[960,2],
 	 	itemsTablet:[768,2],  
-		itemsMobile:[479,1]
+		itemsMobile:[479,1],
 	});
-	$(".link").click(function(){
-		var img = $(this).children().attr('src');
-		img =  img.slice(7);
-		$(".main-img").attr({'src': 'images/big-'+ img});
-	});
-
+	var n = $(".link").length;
+	$("#tasty-0").removeClass("myHide");
 });
 
