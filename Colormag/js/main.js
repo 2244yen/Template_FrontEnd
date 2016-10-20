2b4f531a2e87f5757ccaa470ab4fc7b1495586ee
LinhDang <linhdtt92@gmail.com>
2016-10-20 08:16:40 +0700

    Them project Colormag

---
+++ Colormag/js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    $("#slider4").responsiveSlides({
+        auto: true,
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "centered-btns"
+    });
+
+    var d = new Date();
+    var day = d.getDay();
+    var date = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    var dayArray = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var monthArray = ["January","February","March","April","May","June","July","August","September",
+    "October","November", "December"]
+    var str = dayArray[day] + ", "+ monthArray[month] + " " +  date +  ", " + year;
+    $("#date-now").html(str); 
+
+    $('#sticker').vTicker();
+    $().UItoTop({ easingType: 'easeOutQuart' });
+
+});
