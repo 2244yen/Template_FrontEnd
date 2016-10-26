b55bd903f03a7680e88dbc9ef0a44a4eb2506d6f
LinhDang <linhdtt92@gmail.com>
2016-10-26 17:58:34 +0700

    Edit to we everywhere

--- Moesia/js/main.js
+++ Moesia/js/main.js
@@ -1,3 +1,54 @@
 $(function () {
+	var nav = $(".moesia-nav");
+	$(window).scroll(function() {
+		var height = $(".header").height();
+		if($(this).scrollTop() >= height){
+			nav.addClass('nav-fixed');
+		}
+		else{
+   			nav.removeClass('nav-fixed');
+		}
+	});
     $('.parallax').scrolly({bgParallax: true});
+    new WOW().init();
+    $(".center").slick({
+    	dots: false,
+    	infinite: false,
+    	slidesToShow: 4,
+    	slidesToScroll: 1,
+    	autoplay: true,
+    	autoplaySpeed: 1000,
+    	prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+    	nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+    	responsive: [
+	    	{
+	    		breakpoint: 960,
+	    		settings: {
+	    			slidesToShow: 3,
+	    			slidesToScroll: 1
+	    		}
+	    	},
+	    	{
+	    		breakpoint: 768,
+	    		settings: {
+	    			slidesToShow: 2,
+	    			slidesToScroll: 1
+	    		}
+	    	},
+	    	{
+	    		breakpoint: 480,
+	    		settings: {
+	    			slidesToShow: 2,
+	    			slidesToScroll: 1
+	    		}
+	    	},
+	    	{
+	    		breakpoint: 320,
+	    		settings: {
+	    			slidesToShow: 2,
+	    			slidesToScroll: 1
+	    		}
+	    	}
+    	]
+    });
 });
