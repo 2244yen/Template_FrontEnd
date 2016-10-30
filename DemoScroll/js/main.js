79400f7652618d697c50f1e532846a72c680a013
LinhDang <linhdtt92@gmail.com>
2016-10-30 22:46:12 +0700

    Add file for zoom

--- DemoScroll/js/main.js
+++ DemoScroll/js/main.js
@@ -1,5 +1,38 @@
-$(function () {
-    /**/
-   
-   
-});
+( function( $ ) {
+
+	$window = $(window);
+	$htmlbody = $('html,body');
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections    
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		var s = skrollr.init();
+		
+		// get window size
+	    winH = $window.height();
+	    
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    $slide.height(winH*2);
+	    $('#slide-2, #slide-3').height(winH*3);
+	    
+	    s.refresh($('.homeSlide'));
+	    
+	}
+		
+} )( jQuery );
